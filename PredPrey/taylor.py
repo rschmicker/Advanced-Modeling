4b0d281d43ce5c3af9605f8f26bd2549c2343ffe
Rob Schmicker <rhschmicker@gmail.com>
2018-02-19 22:42:17 -0500

    second order test

--- PredPrey/taylor.py
+++ PredPrey/taylor.py
@@ -4,7 +4,7 @@ import plotly.graph_objs as go
 
 # u(x+h) = u(x) + h(u'(x))
 
-def func(x):
+def u_prime(x):
 	return 3 * x + 2
 
 def main():
@@ -12,11 +12,13 @@ def main():
 	xs = []
 	ys = []
 	ux = 1
+	rounds = 3
 	x_pos = 0
-	for x in range(3):
+	for x in range(rounds):
 		x_pos = x_pos + h
 		xs.append(x_pos)
-		y_pos = ux + h*(func(ux))
+		y_pos = 3*u_prime(ux) + 2*ux
+		#y_pos = ux + h*(u_prime(ux))
 		ys.append(y_pos)
 		ux = y_pos
 		print("x: " + str(x_pos) + " y: " + str(y_pos))
