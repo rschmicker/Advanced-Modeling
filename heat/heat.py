a16025ce331da30f4748d783f5bcfa082c30bd6e
Rob Schmicker <rhschmicker@gmail.com>
2018-03-08 11:28:31 -0500

    fix quick converging

--- heat/heat.py
+++ heat/heat.py
@@ -4,10 +4,15 @@ from numpy import pi
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import sys
+import math
 
 fig = plt.figure()
 fig.set_dpi(100)
 ax1 = fig.add_subplot(1,1,1)
+
+# Heat function
+# def u(x,t):
+# 	return temp0 + np.exp(-diff_const*t)*np.sin(x)
 
 # diffusion constant
 diff_const = 2
@@ -16,39 +21,47 @@ diff_const = 2
 temp0 = 0
 
 # time delta
-k = 0.001
+k = 0.01
+
+iterations = 50
 
 # Known conditions at the edges
 # Evenly spaced increments to 50
-x0, h = np.linspace(0,pi, retstep=True)
+# x0, h = np.linspace(0,pi, retstep=True)
+space, h = np.linspace(0, pi, iterations, retstep=True)
 t0 = 0
-
-# 2D surface (x,t) -> temperate vs. time
-# temperature [0, pi(3.14)]
-# time [0, 10]
-temp = [x0]
-time = [t0]
 
 # multiplier r
 r = k/(h**2)
 
-# Heat function
-def u(x,t):
-	return temp0 + np.exp(-diff_const*t)*np.sin(x)
+def g(x):
+	return math.sin(x)
 
-iterations = 50
+def u(x, t, current_temps):
+	left = current_temps[x - 1]
+	right = current_temps[x + 1]
+	center = current_temps[x]
+	u_next = (1-2*r)*center + r*left + r*right
+	return u_next
+
+x0 = []
+for point in space:
+	x0.append(g(point))
+
+# 2D surface (x,t) -> temperate vs. time
+# temperature [0, pi]
+# time [0, 10]
+temp = [x0]
+time = [t0]
 
 for i in range(iterations):
 	current_temps = temp[i]
 	next_temps = []
-	next_temps.append(0)
+	next_temps.append(0) # zero at the edges
 	for x in range(1, len(current_temps) - 1):
-		left = current_temps[x - 1]
-		right = current_temps[x + 1]
-		center = current_temps[x]
-		u_next = (1-2*r)*u(center, t0) + r*u(left, t0) + r*u(right, t0)
+		u_next = u(x, t0, current_temps)
 		next_temps.append(u_next)
-	next_temps.append(0)
+	next_temps.append(0) # zero at the edges
 	temp.append(next_temps)
 	time.append(t0)
 	t0 += k
@@ -59,7 +72,7 @@ def animate(i):         #The plot shows the temperature evolving with time
     x = temp[p]            #The ends of the rod are kept at temperature temp0
     p += 1              #The rod is heated in one spot, then it cools down
     ax1.clear()
-    plt.plot(x0,x,color='red',label='Temperature at each x')
+    plt.plot(space,x,color='red',label='Temperature at each x')
     plt.plot(0,0,color='red',label='Elapsed time '+str(round(time[p],2)))
     plt.grid(True)
     plt.ylim([temp0,2.5])
