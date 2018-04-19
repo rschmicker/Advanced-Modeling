6ce662cfb7f9babb23c49ad1a212b24d47b9644a
Rob Schmicker <rhschmicker@gmail.com>
2018-04-19 15:55:33 -0400

    semi working

--- PredPrey/part2.py
+++ PredPrey/part2.py
@@ -24,8 +24,8 @@ pygame.display.set_caption("Bacteria and White Blood Cells")
 clock = pygame.time.Clock()
 
 random_range = 5
-bacteria_x = 200
-bacteria_y = 100
+bacteria_x = 400
+bacteria_y = 400
 block_size = 20
 stink_size = 10
 jump_size = 2
@@ -67,8 +67,8 @@ def get_distance(x, y):
     dy = bacteria_y - y
     return hypot(dx, dy)
 
-def get_std_dev(x, y, angle):
-    distance = get_distance(x, y)
+def get_std_dev(x, y, mesh, angle):
+    distance = mesh[x][y] # get_distance(x, y)
     sigma = k * distance**(.5)
     std = get_truncated_normal(mean=angle, sd=sigma, low=0, upp=2*pi)
     return std.rvs()
@@ -88,8 +88,8 @@ def p_model(angle):
     return (int(next_x), int(-next_y))
 
 
-def model(x, y, angle):
-    angle = get_std_dev(x, y, angle)
+def model(x, y, angle, mesh):
+    angle = get_std_dev(x, y, mesh, angle)
     print("Std Angle: " + str(degrees(angle)))
     next_x = jump_size * cos(angle)
     next_y = jump_size * sin(angle)
@@ -108,12 +108,12 @@ reference the temp grid to determine where to go
 max temp from current temp at this cell to swap out for our distance
 
 """
-def diffusion(x,y,t):
+def diffusion_map(x,y,t):
     time_reverse = 10
-    T = 5 # initial temp
+    T = 50 # initial temp
     s = 2 # sigma squared
-    x = np.linspace(-5,5,30)
-    y = np.linspace(-5,5,30)
+    x = np.linspace(-50,50,800)
+    y = np.linspace(-50,50,800)
     x,y = np.meshgrid(x,y)
     return (T/sqrt(1+4*max(time_reverse - t, 0)/s))*exp(-(x**2+y**2)/(s+4*max(time_reverse - t, 0)))
 
@@ -140,34 +140,34 @@ def game_loop():
 
 	# Draw constants
 	window.fill(white)
-	create_stink()
+	# create_stink()
 	create_bacteria()
 
-	print(diffusion(bacteria_x, bacteria_y, t0))
+	diff_map = diffusion_map(bacteria_x, bacteria_y, t0)
 
-	p_rads = get_angle(perfect_x, perfect_y)
-	p_coords = p_model(p_rads)
-	perfect_x += p_coords[0]
-	perfect_y += p_coords[1]
-	pygame.draw.circle(window, green, (perfect_x, perfect_y), block_size)
+	# p_rads = get_angle(perfect_x, perfect_y)
+	# p_coords = p_model(p_rads)
+	# perfect_x += p_coords[0]
+	# perfect_y += p_coords[1]
+	# pygame.draw.circle(window, green, (perfect_x, perfect_y), block_size)
 
 	# check if we are now inside the stink
-	if inside_stink(pos_x, pos_y):
-		# get new angle
-        	rads = get_angle(pos_x, pos_y)
-        	coords = model(pos_x, pos_y, rads)
-        	pos_x += coords[0]
-        	pos_y += coords[1]
-        else:
-    		pos_x += random.randint(-random_range, random_range)
-    		pos_y += random.randint(-random_range, random_range)
+	if 1: # inside_stink(pos_x, pos_y):
+	    # get new angle
+		rads = get_angle(pos_x, pos_y)
+		coords = model(pos_x, pos_y, rads, diff_map)
+		pos_x += coords[0] # + random.randint(-random_range, random_range)
+		pos_y += coords[1] # + random.randint(-random_range, random_range)
+	else:
+		pos_x += random.randint(-random_range, random_range)
+		pos_y += random.randint(-random_range, random_range)
 
-        pos_x, pos_y = within_window(pos_x, pos_y)
-        print("New pos: " + str(pos_x) + ", " + str(pos_y))
-        # DRAW
-        pygame.draw.circle(window, black, (pos_x, pos_y) , block_size)
+	pos_x, pos_y = within_window(pos_x, pos_y)
+	print("New pos: " + str(pos_x) + ", " + str(pos_y))
+	# DRAW
+	pygame.draw.circle(window, black, (pos_x, pos_y) , block_size)
 	pygame.display.update()
-        clock.tick(FPS)
+	clock.tick(FPS)
 	t0 += dt
 
 game_loop()
